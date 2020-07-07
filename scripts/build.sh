e7b5e80d808d467de634432320c07f11496aeca5
Kopilov Aleksandr <kopilov.ad@gmail.com>
2020-07-07 22:24:23 +0300

    Use non-corporative origin

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+#get script placing directory
+PRG="$0"
+while [ -h "$PRG" ]; do
+    ls="$(ls -ld "$PRG")"
+    link="${ls##*-> }" # remove largest prefix: yields link target (behind ->)
+    if [ "$link" != "${link#/}" ]; then # remove prefix / if present
+        # path was absolute
+        PRG="$link"
+    else
+        # was not
+        PRG="$(dirname "$PRG")/$link"
+    fi
+done
+
+DIR="$(dirname "$PRG")"
+
+cd "${DIR}"
+
 rm -f rcallerservice.jar
 #export JAVA_HOME=$HOME/usr/jdk-11.0.6+10
 ../gradlew -b ../build.gradle clean
--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+#get script placing directory
+PRG="$0"
+while [ -h "$PRG" ]; do
+    ls="$(ls -ld "$PRG")"
+    link="${ls##*-> }" # remove largest prefix: yields link target (behind ->)
+    if [ "$link" != "${link#/}" ]; then # remove prefix / if present
+        # path was absolute
+        PRG="$link"
+    else
+        # was not
+        PRG="$(dirname "$PRG")/$link"
+    fi
+done
+
+DIR="$(dirname "$PRG")"
+
+cd "${DIR}"
+
 . build.sh
 
-IMAGE_NAME=gitlab.dellin.ru:5005/forecast/rcallerservice
+IMAGE_NAME=kopilov/rcallerservice
 IMAGE_VERSION=1.2.1
 
-LOCAL_PROXY=http://10.126.6.77:3128
+#LOCAL_PROXY=http://10.126.6.77:3128
 
-docker build --build-arg http_proxy=${LOCAL_PROXY} --build-arg https_proxy=${LOCAL_PROXY} -t ${IMAGE_NAME}:${IMAGE_VERSION} .
-docker push ${IMAGE_NAME}:${IMAGE_VERSION}
+#docker build --build-arg http_proxy=${LOCAL_PROXY} --build-arg https_proxy=${LOCAL_PROXY} -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+#docker push ${IMAGE_NAME}:${IMAGE_VERSION}
 docker image tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}:latest
+#docker push ${IMAGE_NAME}:latest
