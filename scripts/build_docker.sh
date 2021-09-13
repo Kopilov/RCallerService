20aefddb47d308e2f5d878647804c742bb8800c6
Kopilov Aleksandr <kopilov.ad@gmail.com>
2021-09-13 22:04:11 +0300

    Docker fixing

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -18,15 +18,15 @@ DIR="$(dirname "$PRG")"
 
 cd "${DIR}"
 
-. build.sh
-
 IMAGE_NAME=kopilov/rcallerservice
 IMAGE_VERSION=1.1.0
 
 #LOCAL_PROXY=http://10.126.6.77:3128
 
 #docker build --build-arg http_proxy=${LOCAL_PROXY} --build-arg https_proxy=${LOCAL_PROXY} -t ${IMAGE_NAME}:${IMAGE_VERSION} .
-docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} .
-docker push ${IMAGE_NAME}:${IMAGE_VERSION}
-docker image tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}:latest
+docker build --build-arg IMAGE_VERSION=${IMAGE_VERSION} -t ${IMAGE_NAME}:base-${IMAGE_VERSION} -f Dockerfile-base .
+docker build --build-arg IMAGE_VERSION=${IMAGE_VERSION} -t ${IMAGE_NAME}:${IMAGE_VERSION} -f Dockerfile-build_multistage ..
+docker build --build-arg IMAGE_VERSION=${IMAGE_VERSION} -t ${IMAGE_NAME}:forecast-${IMAGE_VERSION} -f Dockerfile-forecast .
+#docker push ${IMAGE_NAME}:${IMAGE_VERSION}
+#docker image tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
+#docker push ${IMAGE_NAME}:latest
